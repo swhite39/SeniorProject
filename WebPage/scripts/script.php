cceba6809191ea0600c1a282587a1e90873d2ed4
Scott White <swhite39@mail.csuchico.edu>
2017-08-21 12:27:18 -0700

    new countdown stuff

--- WebPage/scripts/script.php
+++ WebPage/scripts/script.php
@@ -12,5 +12,29 @@
 	{
 		echo "<li> $line </li>";
 	}*/
-	header("Location: http://localhost/WebPage/test.html");
+	if($data1 == 2)
+	{
+		header("Location: http://localhost/WebPage/countdown2.html");
+	}
+	else if($data1 == 4)
+	{
+		header("Location: http://localhost/WebPage/countdown4.html");
+	}
+	else if($data1 == 6)
+	{
+		header("Location: http://localhost/WebPage/countdown6.html");
+	}
+	else if($data1 == 8)
+	{
+		header("Location: http://localhost/WebPage/countdown8.html");
+	}
+	else if($data1 == 10)
+	{
+		header("Location: http://localhost/WebPage/countdown10.html");
+	}
+	else if($data1 == 12)
+	{
+		header("Location: http://localhost/WebPage/countdown12.html");
+	}
+	
 ?>
